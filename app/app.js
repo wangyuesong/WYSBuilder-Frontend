f41cbc375ee142c16fbceeca8187c099fcc8e446
wangyuesong <573978301@qq.com>
2016-02-12 16:33:34 -0800

    First commit

---
+++ app/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version',
+    'myApp.login'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {redirectTo: '/login'}).when('/login', {templateUrl: 'login/login.html'}).otherwise({redirectTo: '/login'});
+}]);
+
+myApp.controller('myAppController',['$scope','authService',function($scope,authService){
+    $scope.currentUser = null;
+    $scope.isAuthorized = authService.isAuthorized;
+    $scope.setCurrentUser = function(user){
+        $scope.currentUser = user;
+    }
+}])
+//
+//myApp.factory("userInfoFactory", ["$http", function ($http) {
+//    var factory = {};
+//    factory.getUserInfo = function () {
+//        var url = "http://localhost:8080/rest/user?callback=JSON_CALLBACK";
+//        $http.jsonp(url).success(function(data){
+//            return data;
+//        })
+//    }
+//    return factory;
+//}
+//]);
+//
+//myApp.controller("UserInfoController", ["$scope", 'userInfoFactory', function ($scope, userInfoFactory) {
+//    $scope.userInfo = userInfoFactory.getUserInfo();
+//    console.log($scope.userInfo);
+//}]);
+//
---
+++ app/components/session/sessionService.js
@@ -0,0 +1,15 @@
+///**
+// * Created by yuesongwang on 2/10/16.
+// */
+//'use strict';
+//
+//app.factory('sessionService',['',function(){
+//    return {
+//       this.set= function(key, value) {
+//            return sessionStorage.setItem(key, value);
+//        },
+//        get: function (key) {
+//            return sessionStorage.getItem(key);
+//        }
+//    }
+//}])
